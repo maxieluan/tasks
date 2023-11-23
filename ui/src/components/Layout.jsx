c28d812308cdc11e2af8afa22dc5b6c44503c9d6
Max Luan <max_luan@foxmail.com>
2023-11-23 22:40:39 +0800

    full-screen height, collapsable sidebar

--- ui/src/components/Layout.jsx
+++ ui/src/components/Layout.jsx
@@ -1,18 +1,47 @@
 // src/components/Layout.jsx
 
-import { h } from 'preact';
-import Sidebar from './Sidebar';
-import "../styles/main.css"
+import { h } from "preact";
+import Sidebar from "./Sidebar";
+import "../styles/main.css";
 
 const Layout = ({ children }) => {
+  const toggleSidebar = () => {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("collapsed");
+  };
+
+  // Inline CSS styles for the collapsed sidebar
+  const collapsedStyles = `
+        /* CSS for collapsed sidebar */
+        #sidebar.collapsed {
+          width: 50px; /* Adjust the width as needed */
+        }
+    
+        #sidebar.collapsed .icon {
+          font-size: 1.5rem; /* Adjust the font size as needed */
+          white-space: nowrap;
+          overflow: hidden;
+        }
+    
+        #sidebar.collapsed .text {
+          display: none;
+        }
+      `;
   return (
-    <div class="layout">
-      <div class="sidebar">
+    <div class="layout flex h-screen">
+      <aside class="w-1/4 bg-gray-800 flex-shrink-0 transition-all duration-300" id="sidebar">
         <Sidebar />
-      </div>
-      <div class="content">
-        {children}
-      </div>
+      </aside>
+      <section class="w-3/4 p-4 flex-grow">{children}</section>
+      <button
+        id="toggleSidebarButton"
+        class="fixed bottom-0 right-0 p-2 m-2 bg-gray-300 hover:bg-gray-400 transition duration-300"
+        onClick={toggleSidebar}
+    >
+        Toggle Sidebar
+      </button>
+
+      <style>{collapsedStyles}</style>
     </div>
   );
 };
--- ui/src/components/Sidebar.jsx
+++ ui/src/components/Sidebar.jsx
@@ -1,14 +1,20 @@
 // src/components/Sidebar.jsx
 
-import { h } from 'preact';
+import { h } from "preact";
 
 const Sidebar = () => {
   return (
-    <div class="sidebar">
+    <div class="p-5 text-white">
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
       </ul>
     </div>
   );
